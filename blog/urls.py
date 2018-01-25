3e80ea2db0f20ee60fd8c0f0fee77b89420ec746
tz2614 <tony_zheng35@hotmail.com>
2018-01-25 16:40:54 +0000

    changed the html for the blog website.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, re_path
+#from django.urls import path, re_path
+
+from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
 
-	re_path(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.post_list, name='post_list'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+#from django.urls import include, path
+from django.conf.urls import include
+from django.conf.urls import url
 
+#    	path('', include('blog.urls')),
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
+	url(r'^admin/', admin.site.urls),
+	url(r'', include('blog.urls')),
+	]
